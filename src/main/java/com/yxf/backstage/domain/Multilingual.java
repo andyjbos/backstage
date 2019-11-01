6cc20f4e143b48477f31e1243931c10279ac3b7d
andyjbos <andysoftwear@sina.com>
2019-11-01 20:03:54 +0800

    update

--- src/main/java/com/yxf/backstage/domain/Multilingual.java
+++ src/main/java/com/yxf/backstage/domain/Multilingual.java
@@ -1,14 +1,8 @@
 package com.yxf.backstage.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 @Data
-@AllArgsConstructor
-@ToString(callSuper=true)
-@EqualsAndHashCode(callSuper=true)
 public class Multilingual extends ParentUtils {
 
 	private static final long serialVersionUID = -4010451258651702436L;
--- src/main/java/com/yxf/backstage/domain/Users.java
+++ src/main/java/com/yxf/backstage/domain/Users.java
@@ -1,18 +1,12 @@
 package com.yxf.backstage.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 /**
  * 实例类
  * @author win
  */
 @Data
-@AllArgsConstructor
-@ToString(callSuper=true)
-@EqualsAndHashCode(callSuper=true)
 public class Users extends ParentUtils {
 	private static final long serialVersionUID = 5791529580133167492L;
 	private Long id;
